fa74b3dfb5e4852b8a5f1c0a79213e1dcfb88454
EricWithC04 <negativegame117@gmail.com>
2024-04-12 22:28:40 -0300

    fix: environment variables changed

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': 'postgres',
+        # 'USER': os.environ.get('DB_USER'),
+        # 'PASSWORD': os.environ.get('DB_PASSWORD'),
+        # 'HOST': os.environ.get('DB_HOST'),
+        # 'PORT': os.environ.get('DB_PORT'),
     }
 }
 
